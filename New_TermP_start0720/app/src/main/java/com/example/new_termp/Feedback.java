0593fe70f4dfc10ca2a85a13b3e3a1d814e69bf3
Jung <wkengmlwo@gmail.com>
2024-10-28 19:14:03 +0900

    main->free Activity 이동 오류 수정

---
+++ New_TermP_start0720/app/src/main/java/com/example/new_termp/Feedback.java
@@ -0,0 +1,18 @@
+package com.example.new_termp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
